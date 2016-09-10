7156643d311867963bad314d2ac87f0f12bd18c2
hajnina <hajnina11@gmail.com>
2016-09-10 15:47:52 +0200

    hlavičky a patičky
    úklid v kódu... smazány sáhodlouhé vypoznámkované řádky. centralizované
    hlavičky a patičky

---
+++ paticka.php
@@ -0,0 +1,24 @@
+<section id="footer">
+  <ul class="icons">
+    <li><a href="http://twitter.com" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+    <li><a href="http://facebook.com" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+    <li><a href="http://google.com" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+    <li><a href="http://pintrest.com" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+    <li><a href="http://dribble.com" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+    <li><a href="http://linkedin.com" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+  </ul>
+  <div class="copyright">
+    <ul class="menu">
+      <li>&copy; Martin Hajny</li><li>Design: <a href="http://robinsitar.cz">Robin Sitar</a></li>
+    </ul>
+  </div>
+</section>
+
+<!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/jquery.poptrox.min.js"></script>
+  <script src="assets/js/skel.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+  <script src="assets/js/main.js"></script>
---
+++ vypis.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+	<?php include "hlavicka.php"; ?>
+	<body>
+
+
+<?php include "paticka.php"; ?>
+	</body>
+</html>
