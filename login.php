4172de368ffbbcdbf477dc698e5bb2768db5b0e2
Martin Hajný <hajnina11@gmail.com>
2017-10-22 13:13:03 +0200

    Database access

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+-  $admin="adminucet";
+-  $admin_password="adminheslo";
+-  $db_table="tabulka";
+-  $db_address="serverAddress";
+-  $db_username="username";
+-  $db="databaze";
+-  $db_password="databazeHeslo";
+?>
